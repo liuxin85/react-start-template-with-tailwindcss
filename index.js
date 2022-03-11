682be5d6cde026a64444d8ecf731cf5b81dd5d6f
liuxin <liuxin85@gmail.com>
2022-03-11 15:26:19 +0800

    create quiz.md

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-ReactDOM.render(<h1>Hello, React</h1>, document.getElementById("root"));
+function Page() {
+  return (
+    <div>
+      <img src="react.png" alt="" width="50" />
+      <h1>Reasons I like React</h1>
+      <ol>
+        <li>I can find a gool job</li>
+        <li>I can be cool kids again!</li>
+        <li>I can have fun</li>
+      </ol>
+    </div>
+  );
+}
+
+function Page2() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="react.png" alt="" width="50" />
+        </nav>
+      </header>
+      <main>
+        <h1>Why I like React?</h1>
+        <ol>
+          <li>I can find a gool job</li>
+          <li>I can be cool kids again!</li>
+          <li>I can have fun</li>
+        </ol>
+      </main>
+      <footer>
+        <small>@ 2022 Liu development. All rights reserved.</small>
+      </footer>
+    </div>
+  );
+}
+
+ReactDOM.render(<Page2 />, document.getElementById("root"));
---
+++ quiz.md
@@ -0,0 +1,14 @@
+Quiz!
+
+1. What is a React component?
+   A function that returns React element.
+
+2. What's wrong with this code?
+
+```
+function myComponent(){
+    return (
+        <small>I'm tiny text!</small>
+    )
+}
+```
